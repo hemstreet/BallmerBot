b2b7e54ef2c7e5e45a6bd461296d3caeb587b58b
Hemstreet <jonhemstreet@gmail.com>
2016-02-11 20:04:22 -0700

    Moving ballmer bot to rpi

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var gpio = require('pi-gpio-promise');
-var config = require('./config/config');
-var _ = require('underscore');
-var pumps = [];
+"use strict";
 
-_.each(config.pumps, function(value, key) {
-    //
-    pumps[key] = gpio.open(value, "output");
+var express = require('express'),
+    app = express(),
+    Socket = require('./lib/Socket'),
+    config = require('./config/config'),
+    argv = require('yargs').argv,
+    BallmerBot = require('./lib/BallmerBot'),
+    ballmerBot = new BallmerBot(config);
+
+ballmerBot.pour(0,2);
+
+app.use('/', express.static(__dirname + '/app'));
+
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
 });
 
+var server = app.listen(argv.port || config.port, function () {
+    var serverAddress = server.address();
+    var host = serverAddress.address;
+    var port = serverAddress.port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+var io = require('socket.io')(server);
+new Socket(io, server, ballmerBot);
