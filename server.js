69d04dfaad7cf95b5e56c68c268520077e8540a8
Hemstreet <jonhemstreet@gmail.com>
2016-02-11 20:16:56 -0700

    commented out web site

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ var express = require('express'),
 
 ballmerBot.pour(0,2);
 
-app.use('/', express.static(__dirname + '/app'));
-
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
-});
-
-var server = app.listen(argv.port || config.port, function () {
-    var serverAddress = server.address();
-    var host = serverAddress.address;
-    var port = serverAddress.port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-
-var io = require('socket.io')(server);
-new Socket(io, server, ballmerBot);
+//app.use('/', express.static(__dirname + '/app'));
+//
+//app.get('/', function (req, res) {
+//    res.sendFile('/index.html');
+//});
+//
+//var server = app.listen(argv.port || config.port, function () {
+//    var serverAddress = server.address();
+//    var host = serverAddress.address;
+//    var port = serverAddress.port;
+//
+//    console.log('Example app listening at http://%s:%s', host, port);
+//});
+//
+//var io = require('socket.io')(server);
+//new Socket(io, server, ballmerBot);
