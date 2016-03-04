6a3af2d78befbeaed5e4983b64cc10007fc8fab9
Hemstreet <jonhemstreet@gmail.com>
2016-03-03 19:39:06 -0700

    updated mock injection

--- server.js
+++ server.js
@@ -5,7 +5,8 @@
     //Socket = require('./lib/Socket'),
     var config = require('./config/config'),
     argv = require('yargs').argv,
-    BallmerBot = (argv.mock) ? require('./mock/BallmerBot') : require('./lib/BallmerBot'), // Makeshift mock bot
+    lib = (argv.mock) ? 'mock' : 'lib',
+    BallmerBot = require('./' + lib + '/BallmerBot'), // Makeshift mock bot
     ballmerBot = new BallmerBot(config);
 
 console.log(argv);
