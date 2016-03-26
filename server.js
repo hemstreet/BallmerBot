7803a8236c5bb6d9d6f04ed474e432ed86e6925f
Hemstreet <jonhemstreet@gmail.com>
2016-03-26 11:43:08 -0600

    Moved api logic into routes folder

--- server.js
+++ server.js
@@ -10,96 +10,13 @@ config.debug = !!argv.mock;
 
 var ballmerBot = new BallmerBot(config);
 
-
 app.use(express.static('app'));
 
 app.get('/', function (req, res) {
     res.sendFile('/index.html');
 });
 
-app.get('/add/:bottle/:pump', function (req, res) {
-
-    var bottle = req.params.bottle,
-        pump = req.params.pump;
-
-    // Show form to enter bottle data
-        // check for duplicate bottle names ( lowercase )
-
-    res.send({
-        message: 'added bottle ' + bottle + ' at pump ' + pump
-    });
-
-});
-
-app.get('/bottles', function (req, res) {
-
-    // Web request to mongo server for all drinks including [drinks]
-        // return data
-    var bottles = {
-        "pineapple juice" : 0,
-        "coconute cream" : 1,
-        "rum" : 2,
-        "vodka" : 3,
-        "gin" : 4
-    };
-
-
-    res.send({
-        message: bottles
-    });
-
-});
-
-
-app.get('/getAvailableDrinks', function (req, res) {
-    // Web request to mongo server for all drinks including [drinks]
-        // return data
-
-    res.send({
-        message: {
-            "pain killer" : {
-                "pineapple juice" : 2,
-                "coconut cream" : 1,
-                "rum" : 2
-            },
-            "test drink" : {
-                "juice" : 2,
-                "cream" : 1,
-                "gin" : 2
-            }
-        }
-
-    });
-
-});
-
-app.get('/pour/:recipeName', function (req, res) {
-
-    var recipeName = req.params.recipeName;
-
-    // get request for /getAvailableDrinks then match to recipeName
-        // drinks[recipeName]
-        // for each items pass it value
-            ballmerBot.pour(pump, oz);
-
-    // Sorry for partying
-    res.send({
-        message: 'pouring ' + recipeName
-    });
-});
-
-app.get('/pour/:pump/:oz', function (req, res) {
-
-    var pump = req.params.pump,
-        oz = req.params.oz;
-
-    ballmerBot.pour(pump, oz);
-
-    // Sorry for partying
-    res.send({
-        message: 'pouring ' + oz + ' oz from pump ' + pump
-    });
-});
+require('./routes/api')(app, ballmerBot);
 
 var server = app.listen(argv.port || config.port || 8080, function () {
     var serverAddress = server.address();
