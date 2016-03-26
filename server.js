19118cf4c9c7210cd562973a5302876ba6b7d6e9
Hemstreet <jonhemstreet@gmail.com>
2016-03-26 12:49:04 -0600

    fixed help

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-"use strict";
+#!/usr/bin/env node
 
 var express = require('express'),
     app = express(),
     config = require('./config/config'),
     BallmerBot = require('./lib/BallmerBot'),
-    argv = require('yargs').usage('Ballmer Bot - Drink pouring bot\n Usage: $0').options('mock', {
+    argv = require('yargs').usage('Ballmer Bot - Drink pouring bot').options('mock', {
         describe:  'Run the server in mock mode ( simulated Pi )'
     }).options('port', {
         describe:  'Port for the sever to run on'
