dcb2e3d6a4d477286bedef21837e542735b7dc42
Hemstreet <jonhemstreet@gmail.com>
2016-04-10 19:07:42 -0600

    update server ip

--- app/lib/services/drink/drinkService.js
+++ app/lib/services/drink/drinkService.js
@@ -1,6 +1,6 @@
 angular.module('ballmerBot').service('drinkService', ['$http', '$q', function($http, $q) {
 
-    var baseUrl = "http://localhost:3000/api/v1";
+    var baseUrl = "http://10.1.10.18:3000/api/v1";
     var options = {
         headers: {
             'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXNzd29yZCI6InNoYTEkMDY4YmJmYjckMSRiODJjYjBhMTBlYjQ2NTQ4YTBlYjM0ZThjYTc3ZGMwZDJkNzIzODk3IiwiYWRtaW4iOnRydWUsInVzZXJuYW1lIjoiYWRtaW4iLCJfaWQiOiI1NmY3MWNhNWRkYjI3ZGM0ODgwMDAwMDEiLCJfX3YiOjB9.78u-x9WFol9jik_g-HrVF8ocUhYGlGByzYEC_V5N5Lw'
